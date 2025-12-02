98b9b2520d62296b79808b1448e770c16ee4ad6a
shivesh93212 <0105cd231185@oriental.ac.in>
2025-12-02 11:55:04 +0530

    final

--- database.py
+++ database.py
@@ -3,6 +3,10 @@ from sqlmodel import SQLModel, create_engine, Session
 
 DATABASE_URL = os.getenv("DATABASE_URL") or ""
 
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+
 engine = create_engine(DATABASE_URL, echo=True)
 
 def create_db_and_tables():
